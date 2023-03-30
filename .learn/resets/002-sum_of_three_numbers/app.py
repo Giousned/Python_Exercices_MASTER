ff9bcefb37c6223f8c9bb39378df6400c291b1ac
Giousned <guille_ranza@hotmail.com>
2023-03-30 17:54:51 +0000

    [ADD] primeros ejercicios

--- .learn/resets/002-sum_of_three_numbers/app.py
+++ .learn/resets/002-sum_of_three_numbers/app.py
@@ -4,4 +4,4 @@ second_number = int(input("Second input"))
 third_number = int(input("Third input"))
 # print here the sum of three inputs
 
-print(first_number+second_number+third_number)
+print(first_number+second_number)
